34441252ef71620a0a42951fad60892a379d32a5
Balint Alexandru <64432171+balintAlexandru@users.noreply.github.com>
2021-04-15 18:29:16 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache =>{
-            return cache.addAll(["https://balintalexandru.github.io/PWA/index.html",
-                                 "https://balintalexandru.github.io/PWA/script.js",
-                                 "https://balintalexandru.github.io/PWA/style.css",
-                                 "https://balintalexandru.github.io/PWA/style.js",
-                                 "https://balintalexandru.github.io/PWA/logo.png",
-                                 "https://balintalexandru.github.io/PWA/logo1.png",
-                                 "https://balintalexandru.github.io/PWA/manifest.json"
-                                ])
+            return cache.addAll(["/index.html","/script.js","/style.css","/style.js","/logo.png","/logo1.png","/manifest.json"]);
         })
     );
 });
